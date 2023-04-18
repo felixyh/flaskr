774b75dcca848fd3fc4704194d371c1dacd91366
felixyh <felixyanghui@gmail.com>
2023-04-18 13:13:43 +0800

    update

--- boot.sh
+++ boot.sh
@@ -10,4 +10,4 @@ while true; do
     sleep 5
 done
 
-exec gunicorn -b :5000 --access-logfile - --error-logfile - flask/flaskr:flaskr
+exec gunicorn -b :5000 --access-logfile - --error-logfile - flaskr/flaskr:flaskr
