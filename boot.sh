3887e7d023d4c3300007a6bb3d3165cae243304e
felixyh <felixyanghui@gmail.com>
2023-04-18 12:08:37 +0800

    update

---
+++ boot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# this script is used to boot a Docker container
+source venv/bin/activate
+while true; do
+    python -c "from flaskr import init_db; init_db()"
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo init DB failed, retrying in 5 secs...
+    sleep 5
+done
+
+exec gunicorn -b :5000 --access-logfile - --error-logfile - flaskr:app
